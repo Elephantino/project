d85609ea00f871998c2430f6614f518d8bc7664b
Рыбинский Игорь <rybinsky.igor@mail.ru>
2024-03-17 16:39:52 +0800

    commit

--- js/js.js
+++ js/js.js
@@ -457,3 +457,39 @@ textarea.oninput = function () {
   }
 }
 }
+
+
+
+
+
+const list = document.querySelectorAll('.menu li a');
+list.forEach(item => {
+  item.addEventListener('click', (e) => {
+    list.forEach(el => { el.classList.remove('active'); });
+    item.classList.add('active');
+  });
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
